3175b633cc3b338c77d56a4dda50777b8cdca49b
“perl-0815” <chico.poiyo@gmail.com>
2026-02-26 02:26:47 +0900

    iconバグ修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicons/favicon-48x48.png", sizes: "48x48", type: "image/png" },
@@ -52,7 +53,7 @@ export const metadata: Metadata = {
       { url: "/favicons/favicon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
-    shortcut: ["/favicon-32x32.png"],
+    shortcut: ["/favicon.ico"],
   },
 };
 
